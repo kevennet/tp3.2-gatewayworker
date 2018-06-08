d2b501a4033e25ebe1be8c90edd1a5c965184caf
:oaio <2274540015@qq.com>
2018-06-08 17:01:08 +0800

    no commit message

--- Workermen/Applications/start_businessworker.php
+++ Workermen/Applications/start_businessworker.php
@@ -25,7 +25,7 @@ $worker->name = 'ChatBusinessWorker';
 $worker->count = 4;
 // 服务注册地址
 $worker->registerAddress = '127.0.0.1:1236';
-if($_SERVER['GATEWAY_PORT']=7878){
+if($_SERVER['GATEWAY_PORT']==7878){
 	$worker->eventHandler ='push';
 }
 // 如果不是在根目录启动，则运行runAll方法
