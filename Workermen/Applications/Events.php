c0db0e6755dc1ef285b4035537d321d2a163c04e
:oaio <2274540015@qq.com>
2018-06-08 16:45:58 +0800

    no commit message

--- Workermen/Applications/Events.php
+++ Workermen/Applications/Events.php
@@ -220,6 +220,7 @@ class Events
                 
             // 客户端发言 message: {type:say, to_client_id:xx, content:xx}
             case 'say':
+                var_dump($_SERVER);
                 // 非法请求
                 if(!isset($_SESSION['room_id']))
                 {
